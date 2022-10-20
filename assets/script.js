248acb0b3908b31957569b28358bd1d78f117d01
jeremy-fong <112743562+jeremy-fong@users.noreply.github.com>
2022-10-20 00:38:44 -0700

    added color code for timeblocks in javascript

--- assets/script.js
+++ assets/script.js
@@ -9,4 +9,14 @@ function displayCurrentDate() {
     document.getElementById('currentDay').textContent = currentTime.format('dddd, MMMM Do YYYY');
 }
 
+$('.time-block').each(function(){
+    var tbHour = parseInt($(this).attr('id'));
+    if (now > tbHour){
+        $(this).addClass('past')
+    } else if (now < tbHour){
+        $(this).addClass('future')
+    } else if (now === tbHour){
+        $(this).addClass('present')
+    }
+});
 
